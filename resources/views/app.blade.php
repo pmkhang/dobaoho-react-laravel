f346383a2b3c45476722cae63f86dc4c2d76bd43
Pham Minh Khang <pham.m.khang.98@gmail.com>
2024-04-10 12:47:10 +0700

    .

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/x-icon" href="{{asset('uploads/6827194.png')}}">
+    <link rel="icon" type="image/x-icon" href="{{asset('uploads/favicon.png')}}">
     <title inertia>{{ config('app.name', 'Đồ bảo hộ') }}</title>
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
